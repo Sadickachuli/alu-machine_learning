c07f070ad67a20c13db8c2ed3e697d24d2b88835
Sadickachuli <m.achuli@alustudent.com>
2024-05-10 15:07:31 -0700

    Change-scale

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+This code creates a line graph with time on the x-axis and
+the fraction remaining on the y-axis to illustrate the exponential
+decay of C-14 over time. The range of the x-axis is 0 to 28650 years
+, and the y-axis is scaled logarithmically.
+
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 28651, 5730)
+r = np.log(0.5)
+t = 5730
+y = np.exp((r / t) * x)
+
+# Plotting the line graph
+
+plt.plot(x, y, color='blue')
+plt.xlabel('Time (years)')
+plt.ylabel('Fraction Remaining')
+plt.yscale('log')  # This is to set the y-axis to logarithmic scale
+plt.title('Exponential Decay of C-14')
+plt.xlim(0, 28650)  # To range from 0 to 28650
+plt.show()
