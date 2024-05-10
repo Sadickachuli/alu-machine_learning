44f8edd29972fe4860c4e380dba396d6171b17eb
Sadickachuli <m.achuli@alustudent.com>
2024-05-10 12:30:10 -0700

    first

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-'''
-This is a script to plot a line graph with even values from 
+''' This is a script to plot a line graph with even values from
 0 to 10 on the x axis and 0 to 1000(each number raised to power of 3)
 on th y axis.
 '''
@@ -9,10 +8,9 @@ import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
 
-x = np.arange(0, 11) ** 1 #To label even number from 0 to 10
+x = np.arange(0, 11) ** 1  # To label even number from 0 to 10
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.title('Line Graph')
-plt.plot(x, y, color="red") # Plot the axes and use color red for the line
+plt.plot(x, y, color="red")  # Plot the axes and use color red for the line
 plt.show()
-
