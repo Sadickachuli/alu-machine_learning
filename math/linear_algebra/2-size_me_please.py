1dc8bd221f46be07809335fb1f6293e304d9aba3
Sadickachuli <m.achuli@alustudent.com>
2024-05-14 10:07:03 -0700

    funcky

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+
 def matrix_shape(matrix):
-    shape = []
+    """
+    This function computes the shape of a matrix by iterating
+    over its nested lists and appending the length of each dimension
+    to a list called dimension. It then returns this list, which contains
+    the dimensions of the matrix.
+    """
+    dimension = []
     while isinstance(matrix, list):
-        shape.append(len(matrix))
+        dimension.append(len(matrix))
         matrix = matrix[0]
-    return shape
+    return dimension
