95a1b2c784c5f96a599cd21c88bfcd4f424862e3
Sadickachuli <m.achuli@alustudent.com>
2024-05-15 07:35:50 -0700

    flip

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""This will determine the shape of a matrix."""
+
+
+def matrix_transpose(matrix):
+    """
+    Returns the transpose of a 2D matrix.
+    """
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+    transpose_matrix = []
+
+    for j in range(cols):
+        new_row = []
+        for i in range(rows):
+            new_row.append(matrix[i][j])
+        transpose_matrix.append(new_row)
+
+    return transpose_matrix
