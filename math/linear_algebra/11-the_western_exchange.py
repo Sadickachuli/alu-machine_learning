a63d20a6fb122c24912999b4f8cfd016d64fc937
Sadickachuli <m.achuli@alustudent.com>
2024-05-16 06:50:27 -0700

    transposenp

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Function to find the transpose of a matrix with numpy."""
+
+
+def np_transpose(matrix):
+    """
+    Transposes the given matrix.
+    """
+    return matrix.transpose()
