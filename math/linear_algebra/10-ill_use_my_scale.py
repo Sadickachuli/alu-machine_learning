262262b0a59554ebc08ec9296b7e6a935f0b9630
Sadickachuli <m.achuli@alustudent.com>
2024-05-16 06:42:58 -0700

    np-shape

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Function to find the shape of arrays using numpy."""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray.
+    """
+    return matrix.shape
