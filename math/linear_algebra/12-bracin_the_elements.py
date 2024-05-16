3170a14c80f1298fe16e4af41f80fe4f3a2ce414
Sadickachuli <m.achuli@alustudent.com>
2024-05-16 07:02:12 -0700

    elementnps

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -3,16 +3,13 @@
 """Function to perform element-wise operations"""
 
 
-import numpy as np
-
-
 def np_elementwise(mat1, mat2):
     """
     Performs element-wise addition, subtraction, multiplication, and division
     on two numpy arrays.
     """
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
     return add, sub, mul, div
