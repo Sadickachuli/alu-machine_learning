5bf04c93c5a57c9af3a43b0c4278a63e56ad4fac
Sadickachuli <m.achuli@alustudent.com>
2024-05-16 07:43:53 -0700

    saddle

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Function to multiply matrices with numpy"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication using numpy's dot function.
+    """
+    return np.dot(mat1, mat2)
